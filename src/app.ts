872399d32a5b515bb607206e880ce105c61cf2e7
Miltonm Sosa <sosamilton19@gmail.com>
2019-05-22 03:51:48 -0600

    Add app bootstrapping and server implementation files

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import config = require("config");
+import express, { Application } from "express";
+import mongoose from "mongoose";
+import createMiddleware, { SwaggerMiddleware } from "swagger-express-middleware";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerSpecJson from "./config/swagger.json";
+import routes from "./controllers/routes";
+import errorHandler from "./middlewares/error-handler";
+import notFoundHandler from "./middlewares/not-found-handler";
+
+// enable promisified operations
+mongoose.Promise = Promise;
+
+const app: Application = express();
+const uiBasePath: string = config.get("api.uiBasePath");
+const apiBasePath: string = config.get("api.basePath");
+
+createMiddleware(swaggerSpecJson, app, (err, middleware: SwaggerMiddleware) => {
+  app.disable("x-powered-by");
+
+  app.use(express.json());
+  app.use(middleware.metadata());
+  app.use(middleware.parseRequest());
+  app.use(middleware.validateRequest());
+
+  app.use(uiBasePath, swaggerUi.serve, swaggerUi.setup(swaggerSpecJson));
+
+  // registers api routes and redirection from / to {uiBasePath}
+  routes.register(app, apiBasePath, uiBasePath);
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import config from "config";
+import app from "./app";
+import database from "./services/database";
+import logger from "./services/logger";
+
+function startServer() {
+  const port = config.get("server.port");
+
+  app.listen(port, () => {
+      logger.debug("Application listening on: http://localhost:%d", port);
+  });
+}
+
+database.connect()
+  .then(() => {
+    logger.info("Database connection successfully");
+
+    return startServer();
+  })
+  .catch((err) => {
+    logger.error("Unable to connect to database, caused by: %s", err.message || err);
+    process.exit(1);
+  });
